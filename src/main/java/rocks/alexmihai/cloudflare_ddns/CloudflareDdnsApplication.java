294a1c5719d0129fb278ae0477ac018526bf661e
Alex Mihai <49410241+alexmihai1984@users.noreply.github.com>
2024-11-30 18:03:26 +0200

    First commit

---
+++ src/main/java/rocks/alexmihai/cloudflare_ddns/CloudflareDdnsApplication.java
@@ -0,0 +1,13 @@
+package rocks.alexmihai.cloudflare_ddns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflareDdnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudflareDdnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/alexmihai/cloudflare_ddns/CloudflareDdnsApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.alexmihai.cloudflare_ddns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudflareDdnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
